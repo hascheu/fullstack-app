44bc4c1787b2a808519f6c41b568b993924729fa
Hanna Scheudeck <hannascheudeck@Hannas-MacBook-Air.local>
2025-11-09 15:29:54 +0100

    Cors entfernt

--- src/index.js
+++ src/index.js
@@ -1,14 +1,10 @@
 const express = require('express') 
-const cors = require('cors')
 const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
-app.use(cors())
 app.use(morgan('tiny'))
 app.use(express.static('dist'))
-
-
 
 let notes = [
   {
